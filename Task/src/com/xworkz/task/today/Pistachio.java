b53562dcd82d9a04e53db14af876f4b6d094c796
Supriya D <dinakaranctsupriya@gmail.com>
2025-04-09 23:42:17 +0530

    toString

--- Task/src/com/xworkz/task/today/Pistachio.java
+++ Task/src/com/xworkz/task/today/Pistachio.java
@@ -1,4 +1,18 @@
 package com.xworkz.task.today;
 
 public class Pistachio {
+    private String type;
+    private boolean opened;
+    private double price;
+
+    public Pistachio(String type, boolean opened, double price) {
+        this.type = type;
+        this.opened = opened;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "[type=" + type + ", opened=" + opened + ", price=" + price + "]";
+    }
 }
