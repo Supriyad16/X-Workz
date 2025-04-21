deee91db200ef64d62146744f0ba1facaf1df083
Supriya D <dinakaranctsupriya@gmail.com>
2025-04-21 23:39:42 +0530

    Web technology

---
+++ KO19ROM/Web Tech/JS/Index.js
@@ -0,0 +1,20 @@
+var age = 20
+let gender = 'Female'
+const institute= 'x-workz'
+document.writeln(age)
+
+let emp1 ={id:12, employeeName:'aa', salary:100000}
+
+let emp2 = new Object();
+emp2.id=13;
+emp2.employeeName='bb'
+emp2.salary=50000;
+emp2.id=14;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employee=employee;
+    this.salary=salary;
+}
+
+let obj3 = emp3(satisfies, satisfies, s);   
